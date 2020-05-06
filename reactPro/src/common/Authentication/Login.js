e56d22a866f2ab2ec965d26f69a7ddfaba4cc804
kirankumarrr <kirankumarrr>
2020-05-06 23:24:42 +0530

    styling-login-page

--- reactPro/src/common/Authentication/Login.js
+++ reactPro/src/common/Authentication/Login.js
@@ -1,48 +1,57 @@
-import React, {useState} from 'react'
-import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { submitLogin } from "store/auth/action";
+import "./Login.scss";
+const Login = (props) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  // const {a} =useSelector(state=>state.dashboard)
+  const [user, setUser] = useState({ email: "", password: "" });
 
-const Login = props => {
+  const handleSubmit = () => {
+    dispatch(submitLogin(user, history));
+  };
+  const setField = (e) => {
+    const currentUser = Object.assign({}, user);
+    currentUser[e.target.name] = e.target.value;
+    setUser(currentUser);
+  };
 
-    const dispatch = useDispatch();
-    const history = useHistory()
-    // const {a} =useSelector(state=>state.dashboard) 
-    const [user,setUser] = useState({email:'',password:''})
+  return (
+    <div className="login-container login">
+      <div className="input-item">
+        <input
+          type="text"
+          name="email"
+          onChange={(e) => setField(e)}
+          placeholder="Enter your email"
+          value={user.email}
+        />
+        <label htmlFor="email">Email: </label>
+      </div>
+      <div className="input-item">
+        <input
+          type="text"
+          name="password"
+          onChange={(e) => setField(e)}
+          value={user.password}
+          placeholder="Enter your poassword"
+        />
+        <label htmlFor="password">Password:</label>
+      </div>
+      <div className="submit-btn" onClick={handleSubmit}>
+        SingIn
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
 
-    const handleSubmit = () =>{
-        dispatch(submitLogin(user,history));
-    }
-    const setField =(e)=>{
-        const currentUser = Object.assign({},user);
-        currentUser[e.target.name] =e.target.value;
-        setUser(currentUser);
-    }
+Login.propTypes = {};
 
-    return (
-        <div>
-           <label htmlFor="email">
-                Email:
-                <input type="text" name="email"  
-                onChange={e=>setField(e)}
-                value={user.email} />
-            </label>
-            <label htmlFor="password">
-                Password:
-                <input type="text" name="password" 
-                 onChange={e=>setField(e)}
-                value={user.password} />
-            </label>
-            <input type="submit" value="Submit"
-                onClick={handleSubmit}
-            />
-        </div>
-    )
-}
-
-Login.propTypes = {
-
-}
-
-export default Login
+export default Login;

f599423954ea6f198952fccdd22c8941da74822e
kirankumarrr <kirankumarrr>
2020-05-15 23:49:07 +0530

    focus-on-node

---
+++ routes/api/profile.js
@@ -0,0 +1,26 @@
+// express : Route Library - router
+// mogoose : DB Library
+// passport : Authentication Library
+/**
+ *  Validation Function FOR BELOW SECTIONS:
+ *  1) PROFILE
+ *  2) EXPERIENCE
+ *  3) EDUCATION
+*/
+// Profile Model for verification object strucutre
+// User Model fro user object reponse structure
+
+/**
+ * //TODO : API's to Create
+ * GET CURRENT USER PROFILE
+ * GET ALL PROFILE : ADD user Collection details along with that
+ * GET HANDLE by HANDEL :?
+ * GET PROFILE by USER/:userID
+ * POST PROFILE
+ * POST EXPERIENCE FOR PROFILE
+ * POST EDUCATION FOR PROFILE
+ * DELETE EXPERICE FOR PROFILE
+ * DELETE EDUCATION FOR PROFILE
+ * DELTE USER AND PROFILE
+ * 
+*/

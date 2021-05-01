7b5355645b5a2c5a23a9c5c700cae388dff63045
kirankumarrr <kirankumarrr>
2021-05-02 00:24:31 +0530

    fix:responsive navBar

--- reactPro/src/common/NavBar/NavBar.js
+++ reactPro/src/common/NavBar/NavBar.js
@@ -1,27 +1,28 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import { signOut, setCurrentUser } from "store/auth/action";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { signOut, setCurrentUser } from 'store/auth/action';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   HomeTwoTone,
   LoginOutlined,
   LogoutOutlined,
   IdcardOutlined,
-  UsergroupDeleteOutlined
-} from "@ant-design/icons";
+  UsergroupDeleteOutlined,
+  UnorderedListOutlined
+} from '@ant-design/icons';
 
-import "./NavBar.scss";
+import './NavBar.scss';
 
-
-const NavBar = (props) => {
+const NavBar = props => {
+  const [mbNav, setmbNav] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
-  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { isLoggedIn } = useSelector(state => state.auth);
   const signOutBtn = () => {
     dispatch(signOut());
-    history.push("/signIn");
+    history.push('/signIn');
   };
 
   return (
@@ -30,20 +31,24 @@ const NavBar = (props) => {
         <span className="link-text logo-text">FlyHigh</span>
         {/* <HomeTwoTone /> */}
       </Link>
-      <ul className="nav">
+      <UnorderedListOutlined
+        className="navbar-hamberge"
+        onClick={() => setmbNav(r => !r)}
+      />
+      <ul className={`nav ${mbNav ? 'mb' : ''}`}>
         <li className="nav-item">
           <Link to="/signUp" className="nav-link">
-          <UsergroupDeleteOutlined />
+            <UsergroupDeleteOutlined />
             <span className="link-text">Developers</span>
           </Link>
         </li>
         <li className="nav-item">
           <Link to="/signUp" className="nav-link">
-          <IdcardOutlined />
+            <IdcardOutlined />
             <span className="link-text">Sign Up</span>
           </Link>
         </li>
-        
+
         {isLoggedIn ? (
           <li className="nav-item">
             <div className="nav-link" onClick={signOutBtn}>
@@ -51,13 +56,13 @@ const NavBar = (props) => {
               <span className="link-text">Sign Out</span>
             </div>
           </li>
-        ): (
-        <li className="nav-item">
-          <Link to="/signIn" className="nav-link">
-            <LoginOutlined />
-            <span className="link-text">Sign In</span>
-          </Link>
-        </li>
+        ) : (
+          <li className="nav-item">
+            <Link to="/signIn" className="nav-link">
+              <LoginOutlined />
+              <span className="link-text">Sign In</span>
+            </Link>
+          </li>
         )}
       </ul>
     </nav>

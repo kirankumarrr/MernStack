b3ed6d35b7479a1aaa47b7e0409ae6e97a6933f1
kirankumarrr <kirankumarrr>
2021-05-02 19:03:45 +0530

    depl

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,5 @@
-/*
-    * Ignore this file to .gitignore
-*/
-
-module.exports={
-    mongoURI : 'mongodb+srv://kirankumarrr:yg88vw@merncluster.32zbo.mongodb.net/developers?retryWrites=true&w=majority',
-    secretKey: 'secret',
-    google:{
-        clientID :'626280353795-e1oaum5mquenergt2llme8pv2r70popk.apps.googleusercontent.com',
-        clientSecret:'YV7fZGac_AZkWSZEbjLyx9Vs'
-    }
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
---
+++ config/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: process.env.MONDO_URI,
+  secretKey: process.env.SECRET_OR_KEY,
+};

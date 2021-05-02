907c3f7596c5ed78bc3d4c856b836e162a7031b3
kirankumarrr <kirankumarrr>
2021-05-02 20:25:15 +0530

    rename:client folder

--- server.js
+++ server.js
@@ -63,10 +63,10 @@ app.use('/api/posts', posts);
 // Server static assests if in production
 if (process.env.NODE_ENV === 'production') {
   //SET Static folder
-  app.use(express.static('reactPro/build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'reactPro', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

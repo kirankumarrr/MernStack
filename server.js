337eab9ee8a253cfbe5eda1670622d5f1bc9904a
kirankumarrr <kirankumarrr>
2021-05-02 19:20:59 +0530

    pre-for-depl

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Passport:::
 // make verification
@@ -10,17 +10,17 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 //Note when you add bodyParser you need to add middlewares
 // API Routes
-const users =  require('./routes/api/user');
-const profile = require('./routes/api/profile')
-const posts =  require('./routes/api/post')
+const users = require('./routes/api/user');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/post');
 
 const app = express();
 
 //DB config
-const dbPath =  require('./config/keys').mongoURI;
+const dbPath = require('./config/keys').mongoURI;
 
 //Body Parser Middleware
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Passport middlewares
@@ -38,27 +38,38 @@ require('./config/passport')(passport);
  mongodb+srv://kirankumarrr:yg88vw@cluster0-re3gq.mongodb.net/test?retryWrites=true
 */
 
-
-mongoose.connect(dbPath, {
+mongoose
+  .connect(dbPath, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((res)=>{
-    console.log("Connected to MongoDB")
-}).catch(err=>{
-    console.error(err)
-})
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-// Use Routes 
-app.use('/api/users',users);
-app.use('/api/profile',profile);
-app.use('/api/posts',posts);
+//LOAD UI CODE IN HERE
+// Server static assests if in production
+if (process.env.NODE_ENV === 'production') {
+  //SET Static folder
+  app.user(express.static('reactPro/build'));
 
-const port = process.env.PORT || 5000
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'reactPro', 'build', 'index.html'));
+  });
+}
 
-app.listen(port, ()=>console.log(`Server running on port ${port}`));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
